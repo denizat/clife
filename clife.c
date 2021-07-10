eea4f161c4931bf359a3f0c7615a3326852f6553
denizat <deniza.telci@gmail.com>
2021-07-10 11:37:07 -0500

    Did everything

--- clife.c
+++ clife.c
@@ -3,16 +3,12 @@
 #include <unistd.h>
 #include <time.h>
 
-/*
-Todo: change acs to set, allocate tmp diff as the same size as box and just have an instruction counter instead of realloc each time.
-*/
-
-
 int BOX_SIZE;
 int *box;
 
-int* acs(int x, int y) {
-	return &box[x*BOX_SIZE+y];
+void set(int x, int y, int to)
+{
+	box[x*BOX_SIZE+y] = to;
 }
 int view(int x, int y)
 {
@@ -56,7 +52,8 @@ clearBox()
 {
 	for(int i =0; i<BOX_SIZE;i++){
 		for(int k=0;k<BOX_SIZE;k++) {
-			*acs(i,k) = 0;
+			set(i,k,0);
+			/* *acs(i,k) = 0; */
 		}
 	}
 }
@@ -70,7 +67,7 @@ randBox()
 	for(int i =0; i<BOX_SIZE;i++){
 		for(int k=0;k<BOX_SIZE;k++) {
 			if(rand()%10 > 7)
-				*acs(i,k) = 1;
+				set(i,k,1);
 		}
 	}
 }
@@ -155,7 +152,8 @@ iterate() {
 	}
 	printf("%dLEN\n",tmpsize);
 	for(int i = 0; i< tmpsize;i++) {
-		*acs(tmp[i].x,tmp[i].y) = tmp[i].to;
+		/* *acs(tmp[i].x,tmp[i].y) = tmp[i].to; */
+		set(tmp[i].x,tmp[i].y,tmp[i].to);
 		/* printf("i->%d, x->%d, y->%d, to->%d\n",i,tmp[i].x,tmp[i].y,tmp[i].to); */
 	}
 }
@@ -168,7 +166,11 @@ main(int argv, char **argc) {
 		printf("Please give me a box size\n");
 		return 1;
 	}
-	SNEED = atoi(argc[2]);
+	if(argc[2]) {
+		SNEED = atoi(argc[2]);
+	} else {
+		SNEED = 1;
+	}
 	BOX_SIZE = atoi(argc[1]);
 	box = (int *)malloc(BOX_SIZE*BOX_SIZE*sizeof(int));
 	tmp = (todo *)malloc(BOX_SIZE*BOX_SIZE*sizeof(todo));
@@ -177,17 +179,16 @@ main(int argv, char **argc) {
 	tim.tv_nsec = 50000000L;
 	/* tim.tv_nsec = 100000000L; */
 	/* tim.tv_nsec = 100000000L; */
-	/* randBox(); */
-	*acs(0,0)=1;
-	*acs(1,1)=1;
-	*acs(2,1)=1;
-	*acs(0,2)=1;
-	*acs(1,2)=1;
+	set(0,0,1);
+	set(1,1,1);
+	set(2,1,1);
+	set(0,2,1);
+	set(1,2,1);
 	randBox();
-	/* showBox(); */
-	for(;;nanosleep(&tim,NULL)) {
+	for(int i = 0;i<100;nanosleep(&tim,NULL)) {
 		showBox();
 		iterate();
+		i++;
 	}
 	free(tmp);
 	free(box);
